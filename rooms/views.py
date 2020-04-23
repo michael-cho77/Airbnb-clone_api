09f3c83d2a0c3ce15954f7d9cab3152a14f81aaa
Youngchan <michael_cho77@naver.com>
2020-04-23 23:01:20 +0900

    @api_view()
    데코레이터 안에 넣는 http 메소드에 따라 뷰를 볼 수 있음
    # @api_view(['GET', 'POST']) 처럼 동시 허용도 가능

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+# @api_view(['GET', 'POST']) 처럼 동시 허용도 가능
+@api_view(['GET'])
+def list_rooms(request):
+    return Response()
