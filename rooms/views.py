4447280399c0f3175021cb6b0613647408c77694
Youngchan <michael_cho77@naver.com>
2020-04-23 23:51:16 +0900

    Serializers(Hard Coding ver)
    .

--- rooms/views.py
+++ rooms/views.py
@@ -1,8 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Room
+from .serializers import RoomSerializer
 
 
 # @api_view(['GET', 'POST']) 처럼 동시 허용도 가능
 @api_view(['GET'])
 def list_rooms(request):
-    return Response()
+    rooms = Room.objects.all()
+    #many=True = 방 전체(리스트단위)를 직렬화가능하게함
+    serialized_rooms = RoomSerializer(rooms, many=True)
+    return Response(data=serialized_rooms.data)
