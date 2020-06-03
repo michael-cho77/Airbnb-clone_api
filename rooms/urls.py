0943ce340aee59f5815ad25ebc5722b165ed62c9
Youngchan <michael_cho77@naver.com>
2020-06-03 21:41:39 +0900

    ViewSet
    ViewSet또는 ModelViewSet은 이미 존재만으로 create, retrieve와 같은기능을 가지고 있다.

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,12 +1,11 @@
-from django.urls import path
+from rest_framework.routers import DefaultRouter
 from . import views
 
 app_name = "rooms"
+router = DefaultRouter()
+router.register("", views.RoomViewSet)
+
+urlpatterns = router.urls
 
 
 
-urlpatterns = [
-    path("", views.RoomsView.as_view()),
-    path("search/", views.room_search),
-    path("<int:pk>/", views.RoomView.as_view()),
-]
--- rooms/views.py
+++ rooms/views.py
@@ -1,90 +1,34 @@
 from rest_framework.views import APIView
+from rest_framework.viewsets import ViewSet, ModelViewSet
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, permissions
 from rest_framework.decorators import api_view
 from rest_framework.pagination import PageNumberPagination
 from .models import Room
 from .serializers import RoomSerializer
 
 
+class RoomViewSet(ModelViewSet):
 
-class OwnPagination(PageNumberPagination):
-    page_size = 20
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
 
+    def get_permissions(self):
 
-class RoomsView(APIView):
-    def get(self, request):
-        paginator = OwnPagination()
-        '''
-        페이징처리하는 뷰가 많다면 아예 클래스로 선언해도되고 그렇지 않다면 아래처럼 선언해주어야함
-        paginator = PageNumberPagination()
-        paginator.page_size = 20
-        '''
-        rooms = Room.objects.all()
-        # request를 파싱하는것으로 paginator가 page_queryset을 찾게됨
-        results = paginator.paginate_queryset(rooms, request)
-        #request를 통해 누가 RoomsView를 보는지 식별 
-        serializer = RoomSerializer(results, many=True, context={"request": request})
-        # return Response()에서 아래로 바꾸는것으로 이전 이후 페이지등도 사용가능해짐
-        return paginator.get_paginated_response(serializer.data)
-
-    def post(self, request):
-        if not request.user.is_authenticated:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
-        serializer = RoomSerializer(data=request.data)
-        #print(dir(serializer))
-        if serializer.is_valid():
-            # save()안에 들어가는 인자는 validated data안에도 들어간다. 
-            room = serializer.save(user=request.user)
-            room_serializer = RoomSerializer(room).data
-            return Response(data=room_serializer, status=status.HTTP_200_OK)
-        else:
-            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-
-class RoomView(APIView):
-    def get_room(self, pk):
-        try:
-            room = Room.objects.get(pk=pk)
-            return room
-        except Room.DoesNotExist:
-            return None
-
-    def get(self, request, pk):
-        room = self.get_room(pk)
-        if room is not None:
-            serializer = RoomSerializer(room).data
-            return Response(serializer)
+        #list는 get과 같은 즉 /rooms
+        # retrieve는 /rooms 또는 /room/1 과 같음 
+        if self.action =="list" or self.action == "retrieve":
+            # AllowAny 누구나 요청가능함
+            permission_classes = [permissions.AllowAny]
+        #crete = "POST"
+        elif self.action == "create":
+            permission_classes = [permissions.IsAuthenticated]
         else:
             return Response(status=status.HTTP_404_NOT_FOUND)
-
-    def put(self, request, pk):
-        room = self.get_room(pk)
-        if room is not None:
-            if room.user != request.user:
-                return Response(status=status.HTTP_403_FORBIDDEN)
-            # (partial=True)는 모든 데이터가 아닌 내가 원하는 데이터만 수정할 수 있다는 의미.
-            serializer = RoomSerializer(room, data=request.data, partial=True)
-            if serializer.is_valid():
-                room = serializer.save()
-                return Response(RoomSerializer(room).data)
-            else:
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-            return Response()
-        else:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+            permission_classes = [IsOwner]
 
 
-    def delete(self, request, pk):
-        room = self.get_room(pk)
-        if room is not None:
-            if room.user != request.user:
-                return Response(status=status.HTTP_403_FORBIDDEN)
-            room.delete()
-            return Response(status=status.HTTP_200_OK)
-        else:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+
 
 @api_view(["GET"])
 def room_search(request):
